0f57e68aa5b27dc2987eb0fcc85db39f3dd90626
bleach96 <ibzy71@gmail.com>
2023-05-29 15:10:02 -0500

    Added initial implementation of reporting interface and skeleeton for other parts of task 1. Still requires debugging & testing + API access

---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,17 @@
+package com.mindex.challenge.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+
+@Service
+public class ReportingStructureServiceImpl implements ReportingStructureService {
+
+    @Override
+    public ReportingStructure Read(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'Read'");
+    }
+
+}
